85da67cc81b2d4739db4dec734576793a52ddc72
yoonsen <yoonsen@gmail.com>
2025-02-07 19:11:29 +0100

    fixes

--- hello_world.py
+++ hello_world.py
@@ -1,15 +1,22 @@
 import dash
 from dash import html
 
+print("Starting app initialization...")
+
 app = dash.Dash(
-    __name__,
+    __name__,  # Fixed syntax
     serve_locally=True,
     requests_pathname_prefix='/helloworld/',
-    assets_folder='assets',  # Add this
-    assets_url_path='/helloworld/assets'  # And this
+    assets_folder='assets',
+    assets_url_path='/helloworld/assets'
 )
+print("App initialized")
 
 server = app.server
+
+@server.before_request
+def log_request():
+    print("Request received!")
 
 app.layout = html.Div([
     html.H1("Hello, Cloud Run!")
